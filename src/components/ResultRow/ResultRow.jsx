603d934c04e3e4b9f238131bea12a9750d475314
Zehavit <levi.zehavit@gmail.com>
2021-06-17 12:05:44 +0300

    result row component

---
+++ src/components/ResultRow/ResultRow.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ResultRow.scss';
+function ResultRow({data}) {
+    return (
+        <div>
+            {data}
+        </div>
+    );
+}
+
+export default ResultRow;
